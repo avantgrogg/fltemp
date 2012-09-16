d0485596f3770d8844ea015032fed979b3cb1ed9
Doug Grogg <thegroggman@gmail.com>
2012-09-15 22:30:03 -0500

    just checking

---
+++ config/deploy.rb
@@ -0,0 +1,56 @@
+require "bundler/capistrano"
+
+set :application, "forgetlines.com"
+set :repository,  "https://github.com/forgetlines/fltemp.git/trunk"
+set :rvm_ruby_string, 'ruby-1.9.3-p194@fltemp'
+set :rvm_type, :user
+
+set :scm, :subversion
+# Or: `accurev`, `bzr`, `cvs`, `darcs`, `git`, `mercurial`, `perforce`, `subversion` or `none`
+
+role :web, "forgetlines.com"                          # Your HTTP server, Apache/etc
+role :app, "forgetlines.com"                          # This may be the same as your `Web` server
+role :db,  "forgetlines.com", :primary => true # This is where Rails migrations will run
+
+set :deploy_to, "/var/www/vhosts/temp.forgetlines.com"  # CHANGE THIS LINE TO POINT TO THE CORRECT PATH
+set :user, "root"  # CHANGE THIS LINE TO USE YOUR OCS USERNAME
+set :use_sudo, false
+set :port_number, "3002"
+
+after "deploy:update_code", :link_production_db
+
+namespace :deploy do
+	
+  task :start, :roles => :app do
+    run "cd #{deploy_to}/current; passenger start -e production -p #{port_number} -d"
+  end
+  task :stop, :roles => :app do
+    run "cd #{deploy_to}/current; passenger stop -p #{port_number}"
+  end
+  task :restart, :roles => :app do
+    run "cd #{deploy_to}/current; passenger stop -p #{port_number}; passenger start -e production -p #{port_number} -d"
+    run "echo \"WEBSITE HAS BEEN DEPLOYED\""
+  end
+end
+
+# database.yml task
+desc "Link in the production database.yml"
+task :link_production_db do
+  run "ln -nfs #{deploy_to}/shared/config/database.yml #{release_path}/config/database.yml"
+  run "cd #{release_path}; RAILS_ENV=production rake assets:precompile"
+end
+
+# if you want to clean up old releases on each deploy uncomment this:
+# after "deploy:restart", "deploy:cleanup"
+
+# if you're still using the script/reaper helper you will need
+# these http://github.com/rails/irs_process_scripts
+
+# If you are using Passenger mod_rails uncomment this:
+# namespace :deploy do
+#   task :start do ; end
+#   task :stop do ; end
+#   task :restart, :roles => :app, :except => { :no_release => true } do
+#     run "#{try_sudo} touch #{File.join(current_path,'tmp','restart.txt')}"
+#   end
+# end
